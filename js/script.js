094673d17c9629420e0b807b8245c645fc0ceddf
PinkyThomasIG <pinkymukalal@gmail.com>
2024-11-12 17:20:19 +0100

    Update script.js
    Removed the old code. added new variable along with different objects.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-//task 1 pop up menu 
-alert('Hello World'); 
-/* task 1: practing with variables 
-let favouriteFood=('Briyani');
-document.write(favouriteFood); */
+let pokemonList=[
+{
+    pname:'Bulbasaur',
+    height:1.7,
+    types:['grass', 'poison']
+},
+{
+    pname:'Eve',
+    height:0.8,
+    types:['grass', 'poison']
+},
+{
+    pname:'Ivy',
+    height:1,
+    types:['grass', 'posion']
+}
+];
